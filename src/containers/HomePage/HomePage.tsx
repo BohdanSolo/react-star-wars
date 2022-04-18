5510d61014e2dcc6102c002305683203a7000d56
Bohdan <bohdan.solomakha@gmail.com>
2022-04-18 12:55:23 +0300

    Added header and homepage

---
+++ src/containers/HomePage/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ChooseSide from "../../components/HomePage/ChooseSide";
+
+const HomePage = (): JSX.Element => {
+    return (
+        <div>
+            <ChooseSide/>
+        </div>
+    );
+};
+
+export default HomePage;
