278098108faf6139c85e2df415a1e05015825c2b
Bohdan <bohdan.solomakha@gmail.com>
2022-04-18 13:04:33 +0300

    Added error handler using HOC: created errorMessage and notFound page components.

---
+++ src/components/errorMessage/ErrorMessage.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "./ErrorMessage.module.scss";
+import video from "./video/han-solo.mp4";
+import cn from "classnames"
+
+const ErrorMessage = () => {
+  return (
+    <>
+      <p className={cn('error__text', styles.text)}>
+        The dark side of the force has won...
+        <br /> We cannot display data.
+        <br /> Come back when we win and fix everything!
+      </p>
+      <video loop autoPlay muted className={styles.video}>
+        <source src={video} />
+      </video>
+    </>
+  );
+};
+
+export default ErrorMessage;
